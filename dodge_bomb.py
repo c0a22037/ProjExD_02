b663d6c1e76df21d07e0b9787fd8f92320186c87
C0A22037 <c0a2203782@edu.teu.ac.jp>
2023-11-28 17:56:26 +0900

    空白 #Issue2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -24,7 +24,6 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     return yoko, tate
 
 
-
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
